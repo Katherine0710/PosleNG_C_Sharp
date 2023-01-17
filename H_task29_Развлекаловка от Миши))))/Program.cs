a252e132d4ac927185cbaf74f89a823dd6c717e5
Ekaterina <ekaterina.shnaygina02@mail.ru>
2023-01-17 20:12:32 +0300

    29. Ot misyi

---
+++ H_task29_Развлекаловка от Миши))))/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+using System;
+
+class Program 
+{
+    static void Main()
+    {   
+        int[] arr = new int[8];
+        int i = 0;
+        for (;;)
+        {
+            try {
+                arr[i] = new Random().Next();
+                Console.WriteLine(arr[i]);
+            }
+            catch{
+                break;
+            }
+            i++;
+        }
+    }
+}
